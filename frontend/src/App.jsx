2ad01a4a998386f9084c0604962fcf30eb66ae5d
Yash Vardhan <vardhan.yash3105@gmail.com>
2025-07-28 13:45:41 +0530

    v0.2

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,18 @@
-import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import LoginPage from "../src/Pages/LoginPage";
+import RegisterPage from "../src/Pages/RegisterPage";
+import ShopPage from "./Pages/ShopPage";
+import AdminPage from "./Pages/AdminPage";
 
 export default function App() {
   return (
-    <div>
-      <h1 className='text-4xl font-bold text-blue-500 animate-pulse'>
-        🛒 Welcome to E-Commerce Hellstore
-      </h1>
-    </div>
+    <Router>
+      <Routes>
+        <Route path='/' element={<LoginPage />} />
+        <Route path='/register' element={<RegisterPage />} />
+        <Route path='/shop' element={<ShopPage />} />
+        <Route path='/admin' element={<AdminPage />} />
+      </Routes>
+    </Router>
   );
 }
---
+++ frontend/src/Pages/ShopPage.jsx
@@ -0,0 +1,9 @@
+export default function ShopPage() {
+  return (
+    <div className='text-white min-h-screen flex items-center justify-center bg-gray-900'>
+      <h1 className='text-3xl font-bold'>
+        🛒 Welcome to Hellstore. Browse Freely.
+      </h1>
+    </div>
+  );
+}

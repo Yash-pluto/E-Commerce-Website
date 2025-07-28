018740fdbe9ea6a141b184417451eac557a60072
Yash Vardhan <vardhan.yash3105@gmail.com>
2025-07-28 12:48:43 +0530

    v0.1

---
+++ frontend/src/App.jsx
@@ -0,0 +1,11 @@
+import "./App.css";
+
+export default function App() {
+  return (
+    <div>
+      <h1 className='text-4xl font-bold text-blue-500 animate-pulse'>
+        🛒 Welcome to E-Commerce Hellstore
+      </h1>
+    </div>
+  );
+}
